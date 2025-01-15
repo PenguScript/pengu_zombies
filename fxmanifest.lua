f9a8d28c189cc0123040d8cff1c9087c545a5f26
PenguScripts <roccojohn306@gmail.com>
2025-01-14 21:09:20 -0500

    Cleanup fxmanifest.lua by removing unused ped_name.lua and peds.meta references

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,8 +6,7 @@ description 'pengu_zombies'
 version '1.0.0'
 
 client_scripts {
-    'client.lua',
-    'ped_name.lua'
+    'client.lua'
 }
 
 server_script 'server.lua'
@@ -16,12 +15,5 @@ shared_scripts {
     '@ox_lib/init.lua',
     'config.lua',
 }
-
-files {
-    'peds.meta',
-}
-
-data_file 'PED_METADATA_FILE' 'peds.meta'
-
 
 lua54 'yes'
